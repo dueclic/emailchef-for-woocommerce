90712f9f7edd3ebceb603b27bcb9dd33a84ad84f
Alessandro <alessandro@emailchef.com>
2025-02-07 12:04:55 +0100

    [NEW UI] adding list info on left

--- partials/settings/logged-in.php
+++ partials/settings/logged-in.php
@@ -52,6 +52,22 @@ if ( $policy !== 'premium' ) {
             </span>
 
         </div>
+        <hr class="ecwc-hr-separator-half">
+        <div>
+            <div><strong><?php _e( "Emailchef connected list", "emailchef-for-woocommerce" ); ?></strong></div>
+            <div class="ecwc-list-container <?php echo wc_ec_get_option_value( 'list' ) != null ? "ecwc-has-list" : ""; ?>" id="listName">
+                <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" style="height: 16px; margin-top: 2px; display: block" viewBox="0 0 640 512"><path fill="#CCCCCC" d="M96 128a128 128 0 1 1 256 0A128 128 0 1 1 96 128zM0 482.3C0 383.8 79.8 304 178.3 304l91.4 0C368.2 304 448 383.8 448 482.3c0 16.4-13.3 29.7-29.7 29.7L29.7 512C13.3 512 0 498.7 0 482.3zM609.3 512l-137.8 0c5.4-9.4 8.6-20.3 8.6-32l0-8c0-60.7-27.1-115.2-69.8-151.8c2.4-.1 4.7-.2 7.1-.2l61.4 0C567.8 320 640 392.2 640 481.3c0 17-13.8 30.7-30.7 30.7zM432 256c-31 0-59-12.6-79.3-32.9C372.4 196.5 384 163.6 384 128c0-26.8-6.6-52.1-18.3-74.3C384.3 40.1 407.2 32 432 32c61.9 0 112 50.1 112 112s-50.1 112-112 112z"></path></svg>
+
+                </span>
+                <span class="ecwc-list-none ecwc-list-none" id="ecwc-no-list-selected">
+                    <?php _e( "No list connected...", "emailchef-for-woocommerce" ); ?>
+                </span>
+                <span class="truncate ecwc-list-selected" title="test on the fly" id="ecwc-list-selected">
+                    <?php echo wc_ec_get_option_value( 'list' ) ?>
+                </span>
+            </div>
+        </div>
         <hr class="ecwc-hr-separator">
         <div>
             <p><?php _e( "Woocommerce users usually sync automatically with Emailchef. If an issue arises or you need an immediate update, use the button below for manual sync.", "emailchef-for-woocommerce" ); ?></p>
@@ -59,7 +75,7 @@ if ( $policy !== 'premium' ) {
                 <button <?php disabled( wc_ec_get_option_value( 'list' ), null ); ?> type="button"
                                                                                      id="wc_emailchef_sync_now"
                                                                                      class="button button-secondary"
-                title="<?php _e(wc_ec_get_option_value('list') == null ? "Please select a list and save settings first" : "", "emailchef-for-woocommerce") ?>" >
+                                                                                     title="<?php _e(wc_ec_get_option_value('list') == null ? "Please select a list and save settings first" : "", "emailchef-for-woocommerce") ?>" >
                     <?php _e("Manual Sync Now", "emailchef-for-woocommerce"); ?>
                 </button>
             </p>
